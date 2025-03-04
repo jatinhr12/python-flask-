a891ff5e73a0ec0ca42e7dbe4dc5d4f640b91ddb
jatinhr12 <155720501+jatinhr12@users.noreply.github.com>
2025-03-04 07:15:26 +0000

    Added the Flask project

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-
-from flask import Flask
+from flask import Flask 
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, World! Welcome to my Flask application."
+@app.route("/")
+def hello_world():
+  return "India"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
